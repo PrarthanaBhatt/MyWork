a0e90e0f40faa5d7c672abb10b92e7c2441b97da
Prarthana <prarthanabhatt497@gmail.com>
2021-02-26 21:45:00 +0530

    Lifecycle

---
+++ app/src/main/java/com/prarthana/myworkapplication/data_persistence/DataPersistenceMainActivity.java
@@ -0,0 +1,16 @@
+package com.prarthana.myworkapplication.data_persistence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.prarthana.myworkapplication.R;
+
+public class DataPersistenceMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_persistence_main);
+    }
+}
---
+++ app/src/main/java/com/prarthana/myworkapplication/layout/LayoutMainActivity.java
@@ -0,0 +1,16 @@
+package com.prarthana.myworkapplication.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.prarthana.myworkapplication.R;
+
+public class LayoutMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_main);
+    }
+}
